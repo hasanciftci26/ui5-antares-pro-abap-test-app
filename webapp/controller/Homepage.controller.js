350a12d88252e288e00630a6e5c69dd94cdcc569
hasanciftci26 <hasanciftcii26@gmail.com>
2025-08-07 13:08:38 +0200

    UI5 Antares Pro proxy added

--- webapp/controller/Homepage.controller.js
+++ webapp/controller/Homepage.controller.js
@@ -1,10 +1,26 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "ui5/antares/pro/v2/entry/CreateEntry"
+], (Controller, CreateEntry) => {
     "use strict";
 
     return Controller.extend("test.ui5.antares.pro.employeeui.controller.Homepage", {
-        onInit() {
+        onInit: function () {
+            
+        },
+
+        onCreateNewEmployee: function () {
+            const entry = new CreateEntry({
+                controller: this,
+                entitySet: "EmployeeSet",
+                formType: "SimpleForm",
+                propertySettings: [{
+                    name: "FirstName",
+                    required: true
+                }]
+            });
+
+            entry.run();
         }
     });
 });
